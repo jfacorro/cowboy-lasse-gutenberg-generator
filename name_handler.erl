acd48311b9d0cf9cd4a52d940a6150fc0e2c7887
Juan Facorro <juan.facorro@gmail.com>
2014-11-09 14:08:42 -0300

    Added config and lasse file.

---
+++ name_handler.erl
@@ -0,0 +1,25 @@
+-module({{NAME}}_handler).
+-behavior(lasse_handler).
+
+-export([
+	 init/3,
+	 handle_notify/2,
+	 handle_info/2,
+	 handle_error/3,
+	 terminate/3
+	]).
+
+init(_InitArgs, _LastEventId, Req) ->
+    {ok, Req, {}}.
+
+handle_notify(Event, State) ->
+    {send, [{data, <<"some data">>}], State}.
+
+handle_info(_Msg, State) ->
+    {nosend, State}.
+
+handle_error(_Msg, _Reason, State) ->
+    State.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
